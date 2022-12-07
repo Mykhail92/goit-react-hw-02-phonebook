607e05c83b41d0a2ee754f2d252b75b9c5c28085
Mykhail <movemens1992@gmail.com>
2022-12-07 21:25:00 +0200

    complete task 2

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,44 @@
+import { Component } from 'react';
+import { Button, Form, Label } from './ContavtForm.styled';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(
+      e.target.elements.name.value,
+      e.target.elements.number.value
+    );
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Label>Name</Label>
+        <input
+          placeholder="Enter your name"
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+
+        <Label>Number </Label>
+        <input
+          placeholder="Enter your number"
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+
+        <Button>add Contact</Button>
+      </Form>
+    );
+  }
+}
